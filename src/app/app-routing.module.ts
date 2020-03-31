595beb8cc1d74f8aa8870272e89fe3dc7d9e3dd1
Matias Camiletti <matias.camiletti@gmail.com>
2020-03-31 19:57:22 -0300

    Configuracion routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from 'projects/mobileia/layout-coda/src/lib/pages/login-page/login-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent,
+    data: {
+      config: {}
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
