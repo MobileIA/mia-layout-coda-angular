ff3841177206331fd3577b9e6e42bc47a2d39ad3
Matias Camiletti <matias.camiletti@gmail.com>
2020-03-31 19:36:26 -0300

    Creada estructura para la pagina de login.

--- projects/mobileia/layout-coda/src/lib/layout-coda.module.ts
+++ projects/mobileia/layout-coda/src/lib/layout-coda.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { LayoutCodaComponent } from './layout-coda.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 
 @NgModule({
-  declarations: [LayoutCodaComponent],
+  declarations: [LayoutCodaComponent, LoginPageComponent],
   imports: [
   ],
   exports: [LayoutCodaComponent]
