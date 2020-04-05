4639c87eb70eba7ebf7a88e9f3fd64e967d0bba3
Matias Camiletti <matias.camiletti@gmail.com>
2020-04-05 19:08:25 -0300

    Agregada funcionalidad de login.

--- projects/mobileia/layout-coda/src/lib/pages/login-page/login-page.component.ts
+++ projects/mobileia/layout-coda/src/lib/pages/login-page/login-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CodaConfigService } from '../../services/coda-config.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '@mobileia/authentication';
+import { CodaLoginConfig } from '../../entities/coda-login-config';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'coda-login-page',
@@ -8,11 +12,97 @@ import { CodaConfigService } from '../../services/coda-config.service';
 })
 export class LoginPageComponent implements OnInit {
 
+  config: CodaLoginConfig;
+  emailInput = '';
+  passInput = '';
+  messageError = '';
+
   constructor(
-    protected configService: CodaConfigService
+    protected configService: CodaConfigService,
+    private route: ActivatedRoute,
+    private navigator: Router,
+    private authService: AuthenticationService
   ) { }
 
   ngOnInit(): void {
+    this.processConfig();
   }
 
+  onSubmit() {
+    // Limpiar mensaje de error
+    this.messageError = '';
+    // Verificar si ingreso el email
+    if (this.emailInput == '') {
+      this.messageError = 'Debe ingresar el email';
+      return;
+    }
+    if (this.passInput == '') {
+      this.messageError = 'Debe ingresar la contraseña';
+      return;
+    }
+    if (this.config.isInternal) {
+      this.requestInternal();
+    }
+  }
+
+  requestInternal() {
+    this.authService.signInWithEmailAndPasswordInternal(this.emailInput, this.passInput).toPromise().then(data => {
+      if (data.success) {
+
+        if (this.isValidRole(data.response.user.role)) {
+          this.navigator.navigateByUrl(this.config.successRoute);
+        } else {
+          this.messageError = 'Usted no tiene permisos para ingresar';
+          this.authService.signOut();
+        }
+
+      } else {
+        this.messageError = data.error.message;
+      }
+    });
+  }
+
+  processConfig() {
+    this.configService.login.pipe(switchMap(data => {
+      this.config = data;
+      return this.route.queryParams;
+    }))
+    .pipe(switchMap(params => {
+      const redirect = params.redirect;
+      if (redirect !== '/login' && redirect !== '/' && redirect !== '' && redirect !== null
+      && redirect !== undefined && redirect !== '%2F' && redirect !== '/login;redirect=%2F') {
+        this.config.successRoute = redirect;
+      }
+
+      return this.authService.isLoggedIn;
+    }))
+    .pipe(switchMap(isLogged => {
+      if (isLogged) {
+        this.navigator.navigateByUrl(this.config.successRoute);
+      }
+      return this.authService.getAccessToken();
+    }))
+    .subscribe(accessToken => {
+      if (accessToken == null || accessToken == '' || accessToken == undefined) {
+        
+      } else {
+        this.navigator.navigateByUrl(this.config.successRoute);
+      }
+    });
+  }
+
+  /**
+   * Funcion que determinar si el usuario para ingresar necesita un rol especial
+   */
+  isValidRole(userRole: number): boolean {
+    // Si no necesita verificar rol
+    if (!this.config.verifyRole) {
+      return true;
+    }
+    // Verificar si es el mismo rol
+    if (this.config.withRole == userRole) {
+      return true;
+    }
+    return false;
+  }
 }
--- src/app/system/system.component.ts
+++ src/app/system/system.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CodaConfigService } from 'projects/mobileia/layout-coda/src/public-api';
 
 @Component({
   selector: 'app-system',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SystemComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    protected configService: CodaConfigService
+  ) { }
 
   ngOnInit(): void {
+    this.configService.login.next({
+      title: 'Login Test',
+      logoUrl: '',
+      successRoute: '/dashboard',
+      verifyRole: false,
+      withRole: 0,
+      isInternal: true
+    });
   }
 
 }
