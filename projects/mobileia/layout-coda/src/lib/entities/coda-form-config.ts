71eccf1bc7df5e00ad2c3668258f310705b53eb9
Matias Camiletti <matias.camiletti@gmail.com>
2020-04-15 17:26:53 -0300

    Agregada funcionalidad para guardar el formulario y enviar respuesta al cliente.

--- projects/mobileia/layout-coda/src/lib/entities/coda-form-config.ts
+++ projects/mobileia/layout-coda/src/lib/entities/coda-form-config.ts
@@ -1,10 +1,13 @@
 import { CodaFieldConfig } from './coda-field-config';
 import { Subject } from 'rxjs';
 import { Address } from 'ngx-google-places-autocomplete/objects/address';
+import { CodaTableService } from '../services/coda-table.service';
 
 export class CodaFormConfig {
     fields: CodaFieldConfig[];
+    service: CodaTableService<any>;
     item?: any;
 
     onSelectPlace = new Subject<{ field: CodaFieldConfig, place: Address }>();
+    onResponse = new Subject<{ item: any, response: any }>();
 }
