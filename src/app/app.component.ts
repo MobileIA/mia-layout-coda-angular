07b170468a9fbf9cf0bc692d37b6708b150b9556
Matias Camiletti <matias.camiletti@gmail.com>
2020-04-15 16:42:19 -0300

    Ajustes en la pantalla de login.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CodaConfigService } from 'projects/mobileia/layout-coda/src/lib/services/coda-config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Getter IQ';
+
+  constructor(
+    protected configService: CodaConfigService
+  ) { }
+
+  ngOnInit(): void {
+    this.configService.login.next({
+      title: 'Project Test',
+      titleTwo: 'Welcome Back',
+      logoUrl: '',
+      successRoute: '/dashboard',
+      verifyRole: false,
+      withRole: 0,
+      isInternal: true,
+      isLoginWithGoogle: true,
+      backgroundImage: '/assets/img/illustration-login.png'
+    });
+  }
 }
