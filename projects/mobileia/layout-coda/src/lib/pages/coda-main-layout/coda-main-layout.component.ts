8f3bb970eea194dbc4c51f606c64aa4cc2d7ea98
AxelCamiletti <catriel.camiletti@gmail.com>
2020-09-23 18:32:28 -0300

    Fix Page-width with sidebar

--- projects/mobileia/layout-coda/src/lib/pages/coda-main-layout/coda-main-layout.component.ts
+++ projects/mobileia/layout-coda/src/lib/pages/coda-main-layout/coda-main-layout.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CodaSidebarConfig } from '../../entities/coda-sidebar-config';
+import { CodaConfigService } from '../../services/coda-config.service';
 
 @Component({
   selector: 'codamain-layout',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CodaMainLayoutComponent implements OnInit {
 
-  constructor() { }
+  isSidebarOpen = true;
+  config: CodaSidebarConfig;
+
+  constructor(
+    protected configService: CodaConfigService
+  ) { }
 
   ngOnInit(): void {
+    this.observableOpen();
   }
 
+  processConfig() {
+    this.configService.sidebar.subscribe(data => this.config = data);
+  }
+
+  observableOpen() {
+    this.configService.sidebarOpen.subscribe(isOpen => this.isSidebarOpen = isOpen);
+  }
 }
